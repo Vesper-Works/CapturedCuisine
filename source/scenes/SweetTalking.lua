05391446cd0a8cf6cebb552b94ec165b081bc981
Memecon <jon.byrne27@btinternet.com>
2024-06-26 13:13:56 +0100

    Changed file

--- source/scenes/SweetTalking.lua
+++ source/scenes/SweetTalking.lua
@@ -4,9 +4,7 @@ class("SweetTalking").extends(NobleScene)
 
 local scene = SweetTalking
 local pd = playdate
-local left_dialogue
-local right_dialogue
-local middle_dialogue
+
 -- Function to set initial values for the scene
 function scene:setValues()
     self.background = Graphics.image.new("assets/images/background1")
@@ -33,62 +31,41 @@ end
 -- Helper function to setup dialogues
 function scene:setupDialogues()
     local dialogues = self.dialoguebranches[self.currentIngredient]
-    left_dialogue = dialogues["left_dialogue"]
-    middle_dialogue = dialogues["middle_dialogue"]
-    right_dialogue = dialogues["right_dialogue"]
+    self.current_branch = dialogues
 end
+
 
 -- Function to handle updating logic based on user input
 function scene:update()
     if pd.buttonJustReleased(pd.kButtonLeft) then
-        if left_dialogue["success"] == true then
-            print("Left success: " .. left_dialogue["response"])
-            scene:refreshDialogue(left_dialogue)
-        else
-            print("Left response: " .. left_dialogue["response"])
-        end
+        self:processDialogue(self.current_branch["left_dialogue"])
     elseif pd.buttonJustReleased(pd.kButtonRight) then
-        if right_dialogue["success"] == true then
-            print("Right success: " .. right_dialogue["response"])
-            scene:refreshDialogue(right_dialogue)
-        else
-            print("Right response: " .. right_dialogue["response"])
-        end
+        self:processDialogue(self.current_branch["right_dialogue"])
     elseif pd.buttonJustReleased(pd.kButtonUp) then
-        if middle_dialogue["success"] == true then
-            print("Middle success: " .. middle_dialogue["response"])
-            scene:refreshDialogue(middle_dialogue)
-        else
-            print("Middle response: " .. middle_dialogue["response"])
-        end
+        self:processDialogue(self.current_branch["middle_dialogue"])
     end
 end
 
 -- Function to refresh dialogue based on the current branch
-function scene:refreshDialogue(branch)
-    if not branch or not branch["branch"] then
+function scene:processDialogue(dialogue)
+    
+    print(dialogue["response"])
+
+    if not dialogue["branch"] then
         print("No further branches available or branch data is missing.")
         scene:exit()
         return
     end
 
-    local newBranch = branch["branch"]
-    if newBranch == self.current_branch then
-        print("refreshDialogue: Branch did not change, exiting")
-        scene:exit()
-        return
-    end
-
+    local newBranch = dialogue["branch"]
     self.current_branch = newBranch
     print("refreshDialogue: Current branch is " .. tostring(self.current_branch))
+    print("refreshDialogue: left_dialogue: " .. self.current_branch["left_dialogue"]["dialogue"])
+    print("refreshDialogue: middle_dialogue: " .. self.current_branch["middle_dialogue"]["dialogue"])
+    print("refreshDialogue: right_dialogue: " .. self.current_branch["right_dialogue"]["dialogue"])
 
     -- Print out the structure of the current branch for debugging
-
-    if not branch then
-        print("resetDialogues: Branch is nil")
-        return
+    for k, v in pairs(self.current_branch) do
+        print("Branch key: " .. k .. ", value: " .. tostring(v))
     end
-    left_dialogue = newBranch["left_dialogue"]
-    middle_dialogue = newBranch["middle_dialogue"]
-    right_dialogue = newBranch["right_dialogue"]
 end

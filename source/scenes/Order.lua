51fb8ceb26213f7670af2d316e1a0622da0cc5a8
Memecon <jon.byrne27@btinternet.com>
2024-11-01 16:07:55 +0000

    Orders finished

--- source/scenes/Order.lua
+++ source/scenes/Order.lua
@@ -1,6 +1,7 @@
 Order = {}
 local pd = playdate
 local gfx = pd.graphics
+local adjectives = {'crunchy', 'fried', 'grilled', 'happy'}
 class('Order').extends(gfx.sprite) 
 function Order:init(sentence1, sentence2, sentence3, imagePath)
     Order.super.init(self)
@@ -8,17 +9,29 @@ function Order:init(sentence1, sentence2, sentence3, imagePath)
     self.secondSentence = sentence2
     self.thirdSentence = sentence3
     self.path = imagePath
+    self.firstSentence = self:replaceAdjective(self.firstSentence)
+    self.secondSentence = self:replaceAdjective(self.secondSentence)
+    self.thirdSentence = self:replaceAdjective(self.thirdSentence)
 end
 function Order:returnFirstSentence()
+    
     return self.firstSentence
 end
 function Order:returnSecondSentence()
+    
     return self.secondSentence
 end
 function Order:returnThirdSentence()
+    
     return self.thirdSentence
 end
 function Order:returnPath()
     return self.path
+end
+function Order:replaceAdjective(sentence)
+    local adjective = math.random(#adjectives)
+    sentence = sentence:gsub("adjective", adjectives[adjective])
+    print(sentence)
+    return sentence --replace placeholder with required adjective
 end
 --use this class to insert adjectives into the sentences
--- source/scenes/OrdersScene.lua
+++ source/scenes/OrdersScene.lua
@@ -4,18 +4,16 @@ class("OrdersScene").extends(NobleScene)
 local scene = OrdersScene
 local pd = playdate
 local gfx <const> = pd.graphics
-local allOrders = {} --will be filled with new Orders
+local allOrders = {} --will be filled with new Orders, this will stay constant throughout the game, so once the level is finished, this needs to be cleared
+local allDialogue = {} --this will also need to be refilled with all dialogue again
+local currentOrders = 2 --this will increase by one but I'll need to figure out how statics work
 function scene:setValues()
     self.background = Graphics.image.new("assets/images/background1")
     self.color1 = Graphics.kColorBlack
     self.color2 = Graphics.kColorWhite
-    self.a = Order("I want my order to have \n crunchy carrots", "A delightful zesty twist", "Finished with a lime sorbet", "assets/images/bird.png")
-    self.b = Order("I want my order to have \n soggy carrots", "A delightful tangy twist", "Finished with an orange yogurt", "assets/images/Potato.png")
-    table.insert(allOrders, self.a)
-    table.insert(allOrders, self.b)
     self.index = 1;
     Noble.Text.setFont(Noble.Text.FONT_LARGE)
-    self.spriteImage = gfx.image.new(allOrders[self.index]:returnPath())
+    self.spriteImage = gfx.image.new("assets/images/bird.png")
     self.sprite = gfx.sprite.new(self.spriteImage)
     self.sprite:moveTo(50, 200)
     self.sprite:add() --adds to render queue
@@ -23,7 +21,36 @@ end
 
 function scene:init()
     scene.super.init(self) --calls parent constructor
+    self.file = "dialogue/orders.json"
+    self.branch = json.decodeFile(self.file)
+    allDialogue = self.branch.OrderLines
+    self.potentialDialogue = #allDialogue
     self:setValues()
+    self:generateOrders()
+end
+function scene:generateOrders()
+    for i = 1,currentOrders do --generate the required number of orders for each level
+        local ord = Order(self:pickDialogue(), self:pickDialogue(), self:pickDialogue(), "assets/images/bird.png") --pickDialogue called three times to pick ingredient dialogue for order
+        table.insert(allOrders, ord)
+        currentOrders = currentOrders - 1
+    end
+    self:drawAllText(allOrders[1]:returnFirstSentence(), allOrders[1]:returnSecondSentence(), allOrders[1]:returnThirdSentence())
+end
+function scene:drawAllText(firstText, secondText, thirdText)
+    local allText = firstText .. '\n' .. secondText .. '\n' .. thirdText
+    self.textSprite = gfx.sprite.spriteWithText(allText, 400, 200)
+    self.textSprite:moveTo(200, 100)
+    self.textSprite:add()
+end
+function scene:removeAllText()
+    self.textSprite:remove()
+end
+function scene:pickDialogue()
+    local randomNum = math.random(self.potentialDialogue)
+    local dial = allDialogue[randomNum]
+    table.remove(allDialogue, randomNum)
+    self.potentialDialogue = self.potentialDialogue - 1 --randomly picked and then removed from list so that it is not picked again
+    return dial
 end
 function scene:update()
     scene.super.update(self)
@@ -33,16 +60,22 @@ function scene:update()
         else
             self.index = self.index - 1
         end
+        self:removeAllText()
+        self:drawAllText(allOrders[self.index]:returnFirstSentence(), allOrders[self.index]:returnSecondSentence(), allOrders[self.index]:returnThirdSentence())
     elseif pd.buttonJustPressed(pd.kButtonRight) then
         if self.index + 1 > #allOrders then
             self.index = 1
         else
             self.index = self.index + 1
         end
+        self:removeAllText()
+        self:drawAllText(allOrders[self.index]:returnFirstSentence(), allOrders[self.index]:returnSecondSentence(), allOrders[self.index]:returnThirdSentence())
     end
+    --[[
     Noble.Text.draw(allOrders[self.index]:returnFirstSentence(), 0, 0, Noble.Text.ALIGN_LEFT, false, Noble.Text.getCurrentFont()) --it's possible this works but we may need a font asset
     Noble.Text.draw(allOrders[self.index]:returnSecondSentence(), 0, 50, Noble.Text.ALIGN_LEFT, false, Noble.Text.getCurrentFont()) 
     Noble.Text.draw(allOrders[self.index]:returnThirdSentence(), 0, 100, Noble.Text.ALIGN_LEFT, false, Noble.Text.getCurrentFont())
+    --]]
     self.spriteImage = gfx.image.new(allOrders[self.index]:returnPath())
     self.sprite:setImage(self.spriteImage)
     if pd.buttonIsPressed(pd.kButtonB) then
@@ -51,6 +84,7 @@ function scene:update()
 end
 function scene:exit()
     self.sprite:remove()
+    self:removeAllText()
     Noble.Text.setFont(Noble.Text.FONT_MEDIUM)
     Noble.transition(PickIngredientScene, nil, Noble.Transition.DipToBlack)
 end
